35ecedfba40172bbf3da818da7d909faca496f26
leon12-aaryson <aaronoluk4deleonardo@gmail.ccom>
2023-06-06 19:02:39 -0400

    correcting spaces and pycode styles of pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 
+
 def last1(number):
     last_no = abs(number) % 10
     return -last_no if (number < 0) else last_no
 
+
 last = last1(number)
-text = "Last digit of"
+text = "Last digit of "
 
 if last > 5:
-    print(text + " " + str(number) + " " + "is" + " " + str(last) + " " + "and is greater than 5")
+    print(f"LAst digit of {number} is {last}  and is greater than 5")
 elif last == 0:
-    print(text + " " + str(number) + " " + "is" + " " + str(last) + " " + "and is 0")
+    print(f"Last digit of {number} is {last} and is 0")
 else:
-    print(text + " " + str(number) + " " + "is" + " " + str(last) + " " + "and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
