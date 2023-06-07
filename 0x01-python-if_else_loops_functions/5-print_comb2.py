21573bd83ea8a56e97516efba27f1c610476add5
leon12-aaryson <aaronoluk4deleonardo@gmail.ccom>
2023-06-06 21:23:54 -0400

    numerical changes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
