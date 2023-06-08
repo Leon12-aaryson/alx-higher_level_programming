f24d06ad7a81c1fdae025af2237dae33afb50514
leon12-aaryson <aaronoluk4deleonardo@gmail.ccom>
2023-06-08 15:28:36 -0400

    python sun funtion modules trial2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
-result = add(a, b)
-
-print(f"{a} + {b} = " + "{}".format(result), end = "\n")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = " + "{}".format(result), end = "\n")
