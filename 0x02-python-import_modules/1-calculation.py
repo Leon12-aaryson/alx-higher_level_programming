454324327062b252d5f44f02353f773d11fb3d0c
leon12-aaryson <aaronoluk4deleonardo@gmail.ccom>
2023-06-08 16:59:57 -0400

    this is for multiple functions in a single file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,25 @@
-#!/usrbin/python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add
+    a = 10
+    b = 5
+    result = add(a, b)
+    print(f"{a} + {b} = " + "{}".format(result))
+
+    from calculator_1 import sub
+    a = 10
+    b = 5
+    result1 = sub(a, b)
+    print(f"{a} - {b} = " + "{}".format(result1))
+    
+    from calculator_1 import mul
+    a = 10
+    b = 5
+    result2 = mul(a, b)
+    print(f"{a} * {b} = " + "{}".format(result2))
+
+    from calculator_1 import div
+    a = 10
+    b = 5
+    result2 = div(a, b)
+    print(f"{a} / {b} = " + "{}".format(result2))
