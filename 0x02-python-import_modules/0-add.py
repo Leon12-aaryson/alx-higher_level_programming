97fc745886f6bfb3851a55fd5971dbf59ffcc50b
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-08-11 13:01:49 -0700

    adjusting executable mode

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-sum1 = add(a,b)
-print("{:d} + {:d} = {:d}".format(a, b, sum1))
+    a = 1
+    b = 2
+
+    sum1 = add(a,b)
+    print("{:d} + {:d} = {:d}".format(a, b, sum1))
