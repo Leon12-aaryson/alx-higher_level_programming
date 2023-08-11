4a811a15d28a5b39d928ddd6b423f3f42641ebb8
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-08-11 14:06:14 -0700

    output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
+        for i in range(count):
+            print(f"1: {sys.argv[i + 1]}")
     else:
         print("{} arguments:".format(count))
         for i in range(count):
