34872f75b160fe37af50df16a6ec2b10db8ad124
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-08-15 02:31:18 -0700

    rectify the return none issue in max integer function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,12 +2,15 @@
 
 def max_integer(my_list=[]):
     list_len = len(my_list)
-    next_num = my_list[0]
 
     if list_len == 0:
         return None
 
+    next_num = my_list[0]
+
     for i in range(list_len):
         if my_list[i] > next_num:
             next_num = my_list[i]
+    
+
     return next_num
