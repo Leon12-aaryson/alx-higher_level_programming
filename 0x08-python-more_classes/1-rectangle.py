b3c11e5961546d96e0c8aed5bc0c62369f1136fd
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-09-04 11:08:19 -0700

    change angle bracket to mean less then 0

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if value > 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         elif not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -42,6 +42,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value > 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
