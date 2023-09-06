365b43a67decaaf793d45e28dc93324169ad8761
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-09-06 00:07:57 -0700

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python3
-"""Maximum Integer finder """
+"""unittest for max_integer([..]) list
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Returns maximum interger or None """
-    if len(list) == 0:
-        return None
+class TestMaxInteger(unittest.TestCase):
+    """unitests for 6-max_integer"""
+    def test_ints_unorderd(self):
+        """tests for integers only list"""
+        list1 = [1, 2, 7, 3]
+        self.assertEqual(max_integer(list1), 7)
 
-    index = 1
-    
-    max_number = list[0]
-    while index < len(list):
-        if list[index] > max_number:
-            max_number = list[index]
-        index += 1
-    return max_number
+    def test_repeat(self):
+        """checks for max with repeated numbers"""
+        list2 = [1, 2, 2]
+        self.assertEqual(max_integer(list2), 2)
+
+    def test_ordered(self):
+        """checks for max in ordered list"""
+        list3 = [1, 2, 3]
+        self.assertEqual(max_integer(list3), 3)
+
+    def test_empty(self):
+        """check for empty list"""
+        list4 = []
+        self.assertEqual(max_integer(list4), None)
+
+    def test_str(self):
+        """check for string"""
+        list5 = ["emma"]
+        self.assertEqual(max_integer(list5), 'emma')
+
+    def test_float(self):
+        """check for float"""
+        list6 = [1.0, 4.5, 3.7]
+        self.assertEqual(max_integer(list6), 4.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
