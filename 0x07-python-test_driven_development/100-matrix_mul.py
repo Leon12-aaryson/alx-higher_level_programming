c1df37e47e54d9ea1a18204bc8e9c3e1394daa32
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-09-06 06:30:32 -0700

    try check ints

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -32,6 +32,14 @@ def matrix_mul(m_a, m_b):
     if all(not isinstance(nums1, (int, float)) for row in
             m_b for nums1 in row):
         raise TypeError("m_b should contain only integers or floats")
+
+    for row in m_a:
+        if not all(isinstance(item, (int, float)) for item in row):
+            raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        if not all(isinstance(item, (int, float)) for item in row):
+            raise TypeError("m_b should contain only integers or floats")
+
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
