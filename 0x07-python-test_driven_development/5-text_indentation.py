7535a8f6c128a6c08bb9d10e3c7bcded06706a2c
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-09-06 00:11:09 -0700

    new lang

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,7 +13,7 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for character in text:
         if indicator == 1:
             if character == ' ':
