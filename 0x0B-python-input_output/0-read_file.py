262f35b3e37acb00766a355fba361a17b4d8a514
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-09-13 23:15:33 -0700

    using with in function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,12 @@ this is a module for file I/O
 """
 
 
-read_file = __import__('0-read_file').read_file
-read_file("my_file_0.txt")
+def read_file(filename=""):
+    """
+    methd to read file using with
+    Args:
+        filename: this stands for path or name of file
+    """
+    with open(filename, "r", encoding="UTF-8") as read_file:
+        file_details =  read_file.read()
+        print(file_details)
