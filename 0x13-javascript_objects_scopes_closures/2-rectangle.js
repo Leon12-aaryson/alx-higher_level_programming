aff1a0bf8bbdf4d07e8e5ea9f7c247c93f86a5c2
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-11-15 11:07:25 +0300

    new return value for task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+      return 'Rectangle {}';
     }
 
     this.width = w;
