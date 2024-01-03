5b855bffdc013421dd7f5a86ddba65f9435d7e7b
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-01-03 23:28:03 +0300

    semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 const content = process.argv[3];
 const filename = process.argv[2];
 fs.writeFile(filename, content, (err, data) => {
-    if(err){{
-        console.error(err);
-        return;
-    }}
-})
+  if (err) {
+    console.error(err);
+  }
+});
